119168b82f7625b6ecca6bb1d1f62813d00780fa
Andres Botello <felipe.carmona360@gmail.com>
2026-03-02 06:53:03 -0500

    Update Reto1.java

--- Ejerciciosbasicos/src/Reto1.java
+++ Ejerciciosbasicos/src/Reto1.java
@@ -5,32 +5,32 @@ public class Reto1 {
         
         double[] precios = new double[5];
         Scanner datos = new Scanner(System.in);
-        int total = 0;
+        double total = 0;
         double masalta = precios[0];
-        double masbaja = precios[0];
 
         for(int i=1; i<=5; i++){
             System.out.print("Ingrese el precio " + i + ": ");
-            precios[i] = datos.nextInt();
-            total += precios[i];
+            precios[i] = datos.nextDouble();
         }
 
         
 
-        for (int i = 0; i < precios.length; i++) {
-            total += precios[i];
-            if (precios[i] > masalta){
-                masalta = precios[i];
-            } 
-            if (precios[i] < masbaja){
-                masbaja = precios[i];
+        for (double precio : precios) {
+            total += precio;
+            if (precio > masalta){
+                masalta = precio;
             } 
         }
 
+        System.out.println("Total sin descuento:" + total);
         System.out.println("El precio mas alto fue de : $" + masalta);
+        
+        if (total > 20000){
+            System.out.println("Tienes un descuento de 2000");
+            total -=2000;
+        }
 
-
-
+        System.out.println("El total a pagar es de: "+ total);
 
     }
 }
